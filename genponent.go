fd095f2685b8abe4b5fde335c411d62c29742aa6
Tobias S <revolverc24@gmail.com>
2022-02-20 14:25:11 +0900

    Add flag library and flag vars

--- genponent.go
+++ genponent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -9,9 +10,32 @@ func main() {
 
 	fmt.Println(os.Args[0])
 	if len(os.Args) > 1 {
-		componentName := os.Args[1]
-		fmt.Println(componentName)
-		createComponent(componentName)
+
+		var javascriptFlag = flag.Bool("j", false, "Generate a JavaScript component instead of TypeScript")
+		var cssFlag = flag.Bool("c", false, "Generate a component with CSS instead of SCSS")
+		var noStyleFlag = flag.Bool("s", false, "Generate a component with no style")
+		var noPropsFlag = flag.Bool("p", false, "Generate a component without a Props declaration")
+		flag.Parse()
+		remainingArgs := flag.Args()
+
+		if len(remainingArgs) == 0 {
+			fmt.Println("Error: No component name entered")
+			return
+		}
+		// Todo - handle multiple filenames
+		// Todo - --help arg
+
+		fmt.Println("Flag -j value: ", *javascriptFlag)
+		fmt.Println("Flag -c value: ", *cssFlag)
+		fmt.Println("Flag -s value: ", *noStyleFlag)
+		fmt.Println("Flag -p value: ", *noPropsFlag)
+
+		// componentName := os.Args[1]
+		// fmt.Println("Generating React Component: " + componentName)
+		// createComponent(componentName)
+	} else {
+		fmt.Println("Ener a name for your component")
+		fmt.Println("e.g: 'genponent NavBar'")
 	}
 }
 
@@ -43,4 +67,3 @@ func writeCSSFile(componentName string) {
 	defer f.Close()
 	f.WriteString(content)
 }
-
